19e065f39099dcf95e71661fa00a1c6022cd247e
Gabriel Dias <93868997+gabrielcarvalhodias@users.noreply.github.com>
2022-11-03 14:45:02 -0300

    Initial Commit

---
+++ ProjViva/ProjVivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjVivaApp.swift
+//  ProjViva
+//
+//  Created by Gabriel de Carvalho Dias on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjVivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
